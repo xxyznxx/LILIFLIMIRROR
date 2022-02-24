164f8216c77775ad0d4c57502db32add60f576d1
IDONGO <63292417+IDONGO@users.noreply.github.com>
2022-02-24 10:21:27 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -58,7 +58,7 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("𝐎 𝐖 𝐍 𝐄 𝐑", "https://t.me/xxyznxx")
+    buttons.buildbutton("Repo", "https://github.com/IDONGO/LILIFLIMIRROR")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
