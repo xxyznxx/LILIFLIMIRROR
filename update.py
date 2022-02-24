780d5a011f4247ebef3f9b33e6b8a377025587f5
IDONGO <63292417+IDONGO@users.noreply.github.com>
2022-02-24 10:19:53 +0700

    Update update.py

--- update.py
+++ update.py
@@ -49,8 +49,8 @@ if UPSTREAM_REPO is not None:
         srun(["rm", "-rf", ".git"])
 
     srun([f"git init -q \
-            && git config --global user.email e.anastayyar@gmail.com \
-            && git config --global user.name mltb \
+            && git config --global user.email parkchaeyong.chaeyongah@gmail.com \
+            && git config --global user.name IDONGO \
             && git add . \
             && git commit -sm update -q \
             && git remote add origin {UPSTREAM_REPO} \
